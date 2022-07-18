b3a089c282f6c7fcbbbe5f587e0eca216d6ee13b
xili1342 <lucaliu217@outlook.com>
2022-07-18 08:48:42 +0800

    test
    test test

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Redirect, Route, Switch } from 'react-router-dom'
+import {Redirect, Route, Switch} from 'react-router-dom'
 import Home from '../containers/Home'
 import Tables from '../containers/Tables'
 import AsycTables from '../containers/AsycTables'
@@ -31,77 +31,77 @@ import NotFound from '../containers/NotFound'
 import RootBreadcrumb from '../components/layout/RootBreadcrumb'
 import SiderMenus from '../components/layout/SiderMenus'
 
-export const SiderMenusRoute = () => <Route path="*" component={SiderMenus} />
+export const SiderMenusRoute = () => <Route path="*" component={SiderMenus}/>
 
 export const RootBreadcrumbRoute = () => (
-  <Route path="*" component={RootBreadcrumb} />
+    <Route path="*" component={RootBreadcrumb}/>
 )
 
 export const ContentRoute = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/tables" component={Tables} />
-    <Route exact path="/asyc-tables/:pageid" component={AsycTables} />
-    <Redirect from="/asyc-tables" to="/asyc-tables/1" />
-    <Route exact path="/profile" component={Profile} />
-    <Route exact path="/simple-bar-chart" component={SimpleBarChart} />
-    <Route exact path="/simple-area-chart" component={SimpleAreaChart} />
-    <Route exact path="/simple-pie-chart" component={SimplePieChart} />
-    <Route exact path="/simple-line-chart" component={SimpleLineChart} />
-    <Route exact path="/simple-line-chartii" component={SimpleLineChartII} />
-    <Route exact path="/simple-points-chart" component={SimplePointsChart} />
-    <Route exact path="/grouped-bar-chart" component={GroupedBarChart} />
-    <Route exact path="/stacked-bar-chart" component={StackedBarChart} />
-    <Route
-      exact
-      path="/radial-stacked-bar-chart"
-      component={RadialStackedBarChart}
-    />
-    <Route exact path="/simple-chord-chart" component={SimpleChordChart} />
-    <Route
-      exact
-      path="/simple-dendrogram-chart"
-      component={SimpleDendrogramChart}
-    />
-    <Route exact path="/simple-pack-chart" component={SimplePackChart} />
-    <Route exact path="/simple-force-chart" component={SimpleForceChart} />
-    <Route exact path="/simple-voronoi-chart" component={SimpleVoronoiChart} />
-    <Route exact path="/radar-line-chart" component={RadarLineChart} />
-    <Route
-      exact
-      path="/simple-china-map-chart"
-      component={SimpleChinaMapChart}
-    />
-    <Route
-      exact
-      path="/simple-tag-cloud-chart"
-      component={SimpleTagCloudChart}
-    />
-    <Route exact path="/vertical-bp-chart" component={VerticalBPChart} />
-    <Route exact path="/simple-math-chart" component={SimpleMathChart} />
-    <Route
-      exact
-      path="/exponential-spiral-line"
-      component={ExponentialSpiralLine}
-    />
-    <Route
-      exact
-      path="/logarithmic-spiral-line"
-      component={LogarithmicSpiralLine}
-    />
-    <Route
-      exact
-      path="/archimedean-spiral-line"
-      component={ArchimedeanSpiralLine}
-    />
-    <Route
-      exact
-      path="/equiangular-spiral-line"
-      component={EquiangularSpiralLine}
-    />
-    {/* Finally, catch all unmatched routes */}
-    <Route component={NotFound} />
-  </Switch>
+    <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/tables" component={Tables}/>
+        <Route exact path="/asyc-tables/:pageid" component={AsycTables}/>
+        <Redirect from="/asyc-tables" to="/asyc-tables/1"/>
+        <Route exact path="/profile" component={Profile}/>
+        <Route exact path="/simple-bar-chart" component={SimpleBarChart}/>
+        <Route exact path="/simple-area-chart" component={SimpleAreaChart}/>
+        <Route exact path="/simple-pie-chart" component={SimplePieChart}/>
+        <Route exact path="/simple-line-chart" component={SimpleLineChart}/>
+        <Route exact path="/simple-line-chartii" component={SimpleLineChartII}/>
+        <Route exact path="/simple-points-chart" component={SimplePointsChart}/>
+        <Route exact path="/grouped-bar-chart" component={GroupedBarChart}/>
+        <Route exact path="/stacked-bar-chart" component={StackedBarChart}/>
+        <Route
+            exact
+            path="/radial-stacked-bar-chart"
+            component={RadialStackedBarChart}
+        />
+        <Route exact path="/simple-chord-chart" component={SimpleChordChart}/>
+        <Route
+            exact
+            path="/simple-dendrogram-chart"
+            component={SimpleDendrogramChart}
+        />
+        <Route exact path="/simple-pack-chart" component={SimplePackChart}/>
+        <Route exact path="/simple-force-chart" component={SimpleForceChart}/>
+        <Route exact path="/simple-voronoi-chart" component={SimpleVoronoiChart}/>
+        <Route exact path="/radar-line-chart" component={RadarLineChart}/>
+        <Route
+            exact
+            path="/simple-china-map-chart"
+            component={SimpleChinaMapChart}
+        />
+        <Route
+            exact
+            path="/simple-tag-cloud-chart"
+            component={SimpleTagCloudChart}
+        />
+        <Route exact path="/vertical-bp-chart" component={VerticalBPChart}/>
+        <Route exact path="/simple-math-chart" component={SimpleMathChart}/>
+        <Route
+            exact
+            path="/exponential-spiral-line"
+            component={ExponentialSpiralLine}
+        />
+        <Route
+            exact
+            path="/logarithmic-spiral-line"
+            component={LogarithmicSpiralLine}
+        />
+        <Route
+            exact
+            path="/archimedean-spiral-line"
+            component={ArchimedeanSpiralLine}
+        />
+        <Route
+            exact
+            path="/equiangular-spiral-line"
+            component={EquiangularSpiralLine}
+        />
+        {/* Finally, catch all unmatched routes */}
+        <Route component={NotFound}/>
+    </Switch>
 )
 
 export default ContentRoute
